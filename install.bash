8d36a125d41d06a730557089f761d75c3b4f35ab
Sean Tronsen <sean.tronsen@gmail.com>
2023-10-16 09:52:56 -0600

    [main] -- added compatibility fixes for BSD Darwin

--- install.bash
+++ install.bash
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 set -e
+LIB_NAME="bash-common-lib"
+
+# PULL REQUIRED LIBRARIES
+if [ ! -d "$LIB_NAME" ] || [ "$(ls "$LIB_NAME" | wc -l | grep -oE '\d+')" == "0" ]; then
+	git submodule init
+	git submodule update
+fi
+
 source lib.bash
 add-user-bin
 
 files="$(ls -a *.bash)"
 for x in ${files[@]}; do
 	if [ ! "$x" == "lib.bash" ] && [ ! "$x" == "install.bash" ]; then
-		link_name="${x::-5}"
+		echo "issue item: $x"
+
+		# removed parameter expansion due to issues on BSD Darwin
+		link_name="$(sed 's/.bash//' <(echo "$x"))"
 		link_path="$(realpath "$x")"
 		(
 			info "installing '$x' as $link_name"
